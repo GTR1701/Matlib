f6426ea23ec71c701248cad774106f83f90e4e13
GTR1701 <dawidkowal04@gmail.com>
2023-10-21 15:15:29 +0200

    basic addition based calculations & rounding

--- core/functions/multiply.ts
+++ core/functions/multiply.ts
@@ -1,27 +1,49 @@
-export default function multiply(...everyArgument: number[]): number {
+/*
+The MIT License (MIT)
+
+Copyright (c) 2023-2023 Dawid Kowal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+export default function multiply(...everyArgument: any): number {
     const argumentArrayLength = everyArgument.length
-    console.log("argumentArrayLength", argumentArrayLength)
-    let result: number = Array.isArray(everyArgument[0]) ? everyArgument[0][0] : everyArgument[0]
+    let result = Array.isArray(everyArgument[0]) ? everyArgument[0][0] : everyArgument[0]
     const firstArgumentOfTypeArray = Array.isArray(everyArgument[0]) ? true : false
-    console.log("result", result)
-    console.log("firstArgumentOfTypeArray", firstArgumentOfTypeArray)
     for(let iterator = 0; iterator < argumentArrayLength; iterator++){
         if(!firstArgumentOfTypeArray && iterator === 0) continue
+        if(!Array.isArray(everyArgument[iterator]) && typeof everyArgument[iterator] !== "number"){
+            console.error("Variable type not assignable to argument of type number or number[]")
+            return 0
+        }
         if(Array.isArray(everyArgument[iterator])){
-            const currentArray: number[] = everyArgument[iterator]
-            console.log("currentArray", currentArray)
+            const currentArray: any = everyArgument[iterator]
             const internalArrayLength = currentArray.length
-            console.log("internalArrayLength", internalArrayLength)
             for(let internalArrayIterator = 0; internalArrayIterator < internalArrayLength; internalArrayIterator++){
                 if(iterator === 0 && internalArrayIterator === 0) continue
+                if(typeof currentArray[internalArrayIterator] !== "number"){
+                    console.error("Variable type not assignable to argument of type number or number[]")
+                    return 0
+                }
                 result *= currentArray[internalArrayIterator]
-                console.log(result)
             }
         } else {
             if(iterator === 0) continue
-            console.log(everyArgument[iterator])
             result *= everyArgument[iterator]
-            console.log(result)
         }
     }
     return result
--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 import multiply from "./core/functions/multiply";
-const testArray = [2,3,4,5]
-console.log(multiply(2, testArray))
+import power from "./core/functions/power";
+import round from "./core/functions/round";
+import sum from "./core/functions/sum";
+
+console.log("multiply:", multiply(2, [2,3,4,5]))
+console.log("sum:", sum([2,3,4,5]))
+console.log("round", round(3.141521345, "down", 3))
+console.log("power", power(2, 5))
